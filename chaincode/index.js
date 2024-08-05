28faa29a7773c5d22c2221a7d01c15cc1eadfda7
botantantan <fritz_gerald@live.com>
2024-08-05 23:17:31 +0700

    cleanup

--- chaincode/index.js
+++ chaincode/index.js
@@ -1,8 +1,6 @@
 "use strict";
 
 const rxContract = require("./src/RxContract");
-const userContract = require("./src/UserContract");
 
 module.exports.RxContract = rxContract;
-module.exports.UserContract = userContract;
-module.exports.contracts = [rxContract, userContract];
+module.exports.contracts = [rxContract];
